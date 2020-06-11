75b3bd6ea05a90793fd03f861224501244574340
takk <yogotak45@gmail.com>
2020-06-11 22:00:13 +0900

    メインクラスのコミット

---
+++ StepUpGame/src/zenryokuservice/main/game/GameMain.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2019-present ProconServerRPG JavaFX Library All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name ProconServerRPG JavaFX Library nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ */
+package zenryokuservice.main.game;
+
+/**
+ * @author takunoji
+ *
+ * 2020/06/11
+ */
+public class GameMain {
+
+}
